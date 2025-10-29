499d99f39266068403737a827dfef249da4e9d42
FIQRI <aldiansyahfiqri1@gmail.com>
2025-10-29 17:53:56 +0700

    Perbaiki home di main.dart agar ke SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:simoni/splash_screen.dart';
 import 'package:simoni/home_screen.dart';
-<<<<<<< HEAD
-=======
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async { // <-- Ubah jadi 'async'
@@ -11,7 +9,7 @@ void main() async { // <-- Ubah jadi 'async'
   // Inisialisasi manual untuk Android (karena kita tidak pakai FlutterFire CLI)
   await Firebase.initializeApp(); 
   // ---------------------------
->>>>>>> d03e3472e82d79bf4acad355e7d17fc5aa6a78d8
+
 
   runApp(const MyApp());
 }
@@ -24,7 +22,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'SIMONI App',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const HomeScreen(),
+      home: const SplashScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
