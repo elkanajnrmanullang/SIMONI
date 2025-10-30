f23d936bc112089f38804e665d64997f8fa4d163
dindajoycehana <dinda.122140048@student.itera.ac.id>
2025-10-30 09:44:05 +0700

    main.dart balik ke awal

--- lib/main.dart
+++ lib/main.dart
@@ -6,18 +6,14 @@ import 'package:simoni/splash_screen.dart';
 import 'package:simoni/home_screen.dart';
 import 'package:simoni/task_list.dart';
 
-//import 'package:firebase_core/firebase_core.dart';
-//
-//void main() async { // <-- Ubah jadi 'async'
-//  // --- TAMBAHKAN 2 BARIS INI ---
-//  WidgetsFlutterBinding.ensureInitialized();
-//  // Inisialisasi manual untuk Android (karena kita tidak pakai FlutterFire CLI)
-//  await Firebase.initializeApp();
-//  // ---------------------------
-//  runApp(const MyApp());
-//}
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async { // <-- Ubah jadi 'async'
+  // --- TAMBAHKAN 2 BARIS INI ---
+  WidgetsFlutterBinding.ensureInitialized();
+  // Inisialisasi manual untuk Android (karena kita tidak pakai FlutterFire CLI)
+  await Firebase.initializeApp();
+  // ---------------------------
   runApp(const MyApp());
 }
 
@@ -29,7 +25,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'SIMONI App',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const TambahTugasScreen(),
+      home: const LihatTugasScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
