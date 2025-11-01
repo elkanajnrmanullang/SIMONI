26709f0bb0f06aa7581eed1a15b3efac844617c4
Elkana Jnr Manullang <elkanamanullang7@gmail.com>
2025-11-01 14:06:26 +0700

    tes

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:simoni/splash_screen.dart';
-import 'package:simoni/home_screen.dart';
-<<<<<<< HEAD
-=======
 import 'package:firebase_core/firebase_core.dart';
 
-void main() async { // <-- Ubah jadi 'async'
-  // --- TAMBAHKAN 2 BARIS INI ---
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // Inisialisasi manual untuk Android (karena kita tidak pakai FlutterFire CLI)
-  await Firebase.initializeApp(); 
-  // ---------------------------
->>>>>>> d03e3472e82d79bf4acad355e7d17fc5aa6a78d8
-
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -24,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'SIMONI App',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const HomeScreen(),
+      home: const SplashScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
